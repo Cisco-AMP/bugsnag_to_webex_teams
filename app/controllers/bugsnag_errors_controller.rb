6881d72fc517b447bdf3a4b6efdcbaf95bc6cfeb
smsohan <sohan39@gmail.com>
2019-05-02 15:23:52 -0600

    getting closer

--- app/controllers/bugsnag_errors_controller.rb
+++ app/controllers/bugsnag_errors_controller.rb
@@ -5,7 +5,7 @@ class BugsnagErrorsController < ApplicationController
 
   def create
     notification = BugsnagNotifications::Notification.new(params)
-    message = WebexTeams::Message.new(notification.markdown)
+    message = WebexTeams::Message.new(markdown: notification.markdown)
     logger.debug "Sending message: #{notification.markdown} to WebEx to room #{params[:webex_room_id]}"
     message.deliver(params[:webex_room_id])
     render nothing: true, status: :created
